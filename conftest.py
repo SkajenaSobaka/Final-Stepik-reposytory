f573a52d63a2aebf63d9935d0139031fe6de5dac
skajenasobaka <kruciusf2k@mail.ru>
2020-01-12 21:11:02 +0300

    Добавил файл init в соответствии с требованием задания Исправил запуск тестов для браузеров хром и файрфокс добавил возможность выбора языка браузера, по умолчанию - en добавил тестовый сценарий test_main_page

--- conftest.py
+++ conftest.py
@@ -1,20 +1,24 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose lang")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        options = Options()
+        options = ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
@@ -26,4 +30,3 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
-
