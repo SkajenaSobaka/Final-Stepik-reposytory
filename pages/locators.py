54936877589115a1a8388d9c032ea55a1bacec66
skajenasobaka <kruciusf2k@mail.ru>
2020-01-16 21:40:00 +0300

    Добавил файл locators, который содержит необходимые локаторы в одном месте + добавил обхект login_page с проверками наличия урл, формы регистрации и формы авторизации

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_URL = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
